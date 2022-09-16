405adac72dd79e616982b321c31d939216cead36
minjuuH <hwangmj1124@gmail.com>
2022-09-17 00:12:26 +0900

    초기 커밋.

---
+++ chapter01/HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class HelloWorld
+{
+    public static void Main()       //main이 아니라 Main임에 주의
+    {
+        Console.WriteLine("HelloWorld!");
+    }
+}
---
+++ chapter01/WinFormApp/WinFormApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms; //그냥 입력했을 때 참조 오류가 발생 -> 참조에 System.Windows.Forms.dll을 추가해줌
+
+class WinFormApp : Form     //Form 클래스 상속
+{
+    WinFormApp()    //생성자
+    {
+        this.Text = "Hello!";  //타이틀 이름
+    }
+    public static void Main()
+    {
+        Application.Run(new WinFormApp());  //윈도우창 생성
+        //Application : 애플리케이션을 시작 및 중지하기 위한 메서드
+        //Application.Run(Form) : 애플리케이션 루프를 실행하고 지정한 Form을 표시
+    }
+}
