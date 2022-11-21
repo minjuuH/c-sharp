058311316f7eb4a88c03735c8177c70b212b8c5e
minjuuH <hwangmj1124@gmail.com>
2022-11-21 23:52:49 +0900

    chapter 12 예제
    winform에서 Pen 객체를 사용하여 사각형 그리기 작업 연습

---
+++ chapter 12/PenApp/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PenApp {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Pen p = new Pen(Color.Black, 20);   //생성자에서 pen width 지정
+            // p.Width = 10;
+            g.DrawRectangle(p, 30, 30, 70, 70);
+            p.Dispose();    //Pen 자원 반납
+        }
+    }
+}
