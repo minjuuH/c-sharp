9d4e88f207d0fd5ef917e569abf6aaa00404f6e5
minjuuH <hwangmj1124@gmail.com>
2022-11-21 23:28:48 +0900

    chapter 12 예제
    확대되는 화면에 랜덤한 색을 칠해주는 예제

---
+++ chapter 12/ch12-01/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ch12_01 {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Random r = new Random();
+            Color c = Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
+            //Random.Next() : 0 ~ 2^(32-1) 사이의 난수 값 생성
+            //Random.Next(int1=0, int2) : int1 ~ int2 사이의 난수 값 생성
+
+            g.FillRectangle(new SolidBrush(c), e.ClipRectangle);
+        }
+    }
+}
