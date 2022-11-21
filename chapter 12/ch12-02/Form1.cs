1a2b5d9d1fec63d71f955c73cf8bb2fd1c3f0730
minjuuH <hwangmj1124@gmail.com>
2022-11-21 23:32:54 +0900

    chapter 12 예제
    폼을 클릭할 때마다 랜덤한 색으로 화면이 칠해지는 예제
    ch12-01과 달리 창의 크기를 확대할 때 랜덤한 색으로 확대된 부분이 채워지지 않는다

---
+++ chapter 12/ch12-02/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ch12_02 {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Random r = new Random();        //난수를 생성할 랜덤 객체 생성
+            Color c = Color.FromArgb(r.Next(0, 256), r.Next(0, 256), r.Next(0, 256));   //랜덤 색상 지정
+            g.FillRectangle(new SolidBrush(c), ClientRectangle);
+        }
+    }
+}
