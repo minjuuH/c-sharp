b43b7fdfa3dbda8654b8eb6468b781ab69f4c31b
minjuuH <hwangmj1124@gmail.com>
2022-11-24 00:51:18 +0900

    chapter 12 예제
    기준점을 여러개 사용하여 색 전환을 하는 PathGradientBrush 사용 예제

---
+++ chapter 12/PathGradientBrushApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace PathGradientBrushApp {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+
+            //기준점으로 사용할 point 배열
+            Point[] pts = { new Point(ClientRectangle.Width/2, 0),
+                new Point(0, ClientRectangle.Height),
+                new Point(ClientRectangle.Width, ClientRectangle.Height) };
+
+            PathGradientBrush b = new PathGradientBrush(pts);   //기준점 여러개 색전환
+            g.FillRectangle(b, ClientRectangle);
+            b.Dispose();
+
+        }
+    }
+}
