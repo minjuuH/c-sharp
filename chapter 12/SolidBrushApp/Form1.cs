1e3d3dd1ac20f325747f879efcb2baea66d6f801
minjuuH <hwangmj1124@gmail.com>
2022-11-24 00:42:36 +0900

    chapter 12 예제
    pen 객체를 생성하여 단색으로 화면을 채우는 예제

---
+++ chapter 12/SolidBrushApp/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SolidBrushApp {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            SolidBrush b = new SolidBrush(Color.Lime);  //연두색 지정
+            g.FillRectangle(b, ClientRectangle);        //전체 채우기
+            b.Dispose();
+        }
+    }
+}
