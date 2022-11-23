c3d412d7b0897498927c52e34d04197422e813ff
minjuuH <hwangmj1124@gmail.com>
2022-11-24 00:49:22 +0900

    chapter 12 예제
    선형 색 전환으로 면을 칠하는 LinearGradientBrush 객체 사용 예제

---
+++ chapter 12/LinearGradientApp/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace LinearGradientApp {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            LinearGradientBrush b = new LinearGradientBrush(new Point(0, 0), new Point(40, 30), Color.Blue, Color.Red);
+            //LinearGradientBrush(시작점, 끝점, 시작 색, 끝 색)
+            g.FillRectangle(b, ClientRectangle);
+            b.Dispose();
+        }
+    }
+}
