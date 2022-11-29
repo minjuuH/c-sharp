bafd1a274cc4f09addb76ad3b5ff8ea15069f193
minjuuH <hwangmj1124@gmail.com>
2022-11-29 23:43:35 +0900

    chapter 10 예제
    ch10-02
    메뉴 항목 이벤트 처리 예제

---
+++ chapter 10/ch10-02/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ch10_02 {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //버튼 이벤트
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //메뉴 이벤트
+        private void 새파일NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+        private void 열기NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+        private void 닫기NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+        private void 저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+        private void 다른이름으로저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+        private void 인쇄ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+        private void 미리보기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+        private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+        private void 잘라내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+        private void 복사ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+        private void 붙여넣기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+        private void 정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+    }
+}
