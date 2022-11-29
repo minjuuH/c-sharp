d480969d210d9c1ae42be4fe87bfaec2f9270a85
minjuuH <hwangmj1124@gmail.com>
2022-11-29 23:07:31 +0900

    chapter 12 예제
    DrawLineApp-8
    점을 연결하는 폐곡선 그리기

---
+++ chapter 12/DrawLineApp-8/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawLineApp_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            // 폐곡선(닫힌 곡선) 그리기
+            Graphics g = CreateGraphics();
+            Point[] pts = { new Point(115, 30), new Point(140, 90),
+                new Point(200, 115), new Point(140, 140),
+                new Point(115, 200), new Point(90, 140),
+                new Point(30, 115), new Point(90, 90) };
+            g.FillClosedCurve(Brushes.Yellow, pts);
+            g.DrawClosedCurve(Pens.Red, pts);
+        }
+    }
+}
