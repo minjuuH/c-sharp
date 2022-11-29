6b9a91931a9cde61ca70d3c051e26ba47bab80a1
minjuuH <hwangmj1124@gmail.com>
2022-11-29 23:16:17 +0900

    chapter 12 예제
    DrawStringApp
    DrawString 메소드 사용 예제

---
+++ chapter 12/DrawStringApp/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawStringApp {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Font f = new Font("Tahoma", 15);
+            g.DrawString("Hello World!", f, Brushes.Black, 10, 10);
+            f.Dispose();
+
+        }
+    }
+}
