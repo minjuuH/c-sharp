721bd601d6bbd13ff72639dc848f5a7fc6efcdbb
minjuuH <hwangmj1124@gmail.com>
2022-11-29 23:48:28 +0900

    chapter 10 예제
    ch10-03
    MouseWheel 이벤트 처리 예제

---
+++ chapter 10/ch10-03/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ch10_03
+{
+    public partial class Form1 : Form
+    {
+        private int num;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //마우스휠 이벤트 등록 방법
+            this.MouseWheel += new MouseEventHandler(this.Form1_MouseWheel);
+        }
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = (e.Delta > 0 ? num++ : num--).ToString();
+        }
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            num = 0;
+            textBox1.Text = num.ToString();
+        }
+
+    }
+}
