19481f2a5791e1808a9e60a0236c6457f0e93348
minjuuH <hwangmj1124@gmail.com>
2022-11-29 23:10:02 +0900

    chapter 12 예제
    DrawLineApp-9
    베지어 곡선 그리기 예제

---
+++ chapter 12/DrawLineApp-9/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawLineApp_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            // 베지어 곡선 그리기(1)
+            //베지어 곡선: 시작점, 제어점 2개, 끝 점을 받아서 곡선을 그림 / 곡선이 제어점을 지나가지 않음
+            Graphics g = CreateGraphics();
+            g.DrawBezier(Pens.Magenta,
+            new Point(100, 50), // start point
+            new Point(0, 100), // control point one
+            new Point(200, 100), // control point two
+            new Point(100, 150)); // end point
+        }
+    }
+}
