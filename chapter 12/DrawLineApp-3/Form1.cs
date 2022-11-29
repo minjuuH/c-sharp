4a17e9905581827a80c736393721fb6a1aad32fd
minjuuH <hwangmj1124@gmail.com>
2022-11-29 22:55:44 +0900

    chapter 12 예제
    DrawLineApp-3
    FillRectangle 메소드로 사각형 채우기 / DrawRectangle 메소드로 사각형 그리기 예제

---
+++ chapter 12/DrawLineApp-3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawLineApp_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            // 사각형 그리기
+            Graphics g = CreateGraphics();
+            Rectangle r = new Rectangle(50, 50, 150, 100);
+            g.FillRectangle(Brushes.Lime, r);
+            g.DrawRectangle(new Pen(Color.Black), r);
+        }
+    }
+}
