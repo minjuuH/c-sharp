4616770b1b7d9c45b27364a90e7410ea19545d6b
minjuuH <hwangmj1124@gmail.com>
2022-11-29 23:41:50 +0900

    chapter 10 예제
    ch10-01
    메뉴 항목 작성 예제

---
+++ chapter 10/ch10-01/Form1.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Forms;
+
+namespace MenuSample {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
