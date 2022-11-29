7ecbb142d80174f697aa71ad2c4a7e65e0bb5272
minjuuH <hwangmj1124@gmail.com>
2022-11-29 22:49:32 +0900

    chapter 12 예제
    DrawLineApp-1
    DrawLine 메소드로 엑스표를 그리는 예제

---
+++ chapter 12/DrawLineApp-1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawLineApp_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Pen p = new Pen(Color.Black);
+            Point startPoint = new Point(45, 45);
+            Point endPoint = new Point(180, 150);
+            g.DrawLine(p, startPoint, endPoint);
+            g.DrawLine(p, new Point(190, 60), new Point(65, 170));
+            p.Dispose();
+        }
+    }
+}
