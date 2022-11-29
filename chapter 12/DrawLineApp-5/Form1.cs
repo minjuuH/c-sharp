ff4ed137dc3a1a9c4fd2d69474f90832e6cf70a0
minjuuH <hwangmj1124@gmail.com>
2022-11-29 22:59:39 +0900

    chapter 12 예제
    DrawLineApp-5
    타원 그리는 DrawEillipse() / FillEllipse() 메소드 사용 예제

---
+++ chapter 12/DrawLineApp-5/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawLineApp_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            // 타원 그리기 - 사각형에 내접하는 타원 그리기
+            Graphics g = CreateGraphics();
+            Rectangle r = new Rectangle(50, 50, 150, 100);
+            g.FillEllipse(Brushes.Cyan, r);
+            g.DrawEllipse(Pens.Black, r);
+        }
+    }
+}
