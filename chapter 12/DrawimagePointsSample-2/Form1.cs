9fa8a4969796a41d824ed9f2db1f6dabf9dc6a5b
minjuuH <hwangmj1124@gmail.com>
2022-11-29 23:35:33 +0900

    chapter 12 예제
    DrawimagePointsSample-2
    이미지 그리기 예제

---
+++ chapter 12/DrawimagePointsSample-2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawimagePointsSample_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            //이미지를 뒤집도록 설정
+            Graphics g = CreateGraphics();
+            Image img = new Bitmap("C:\\Users\\hwang\\source\\repos\\C#\\chapter 12\\vs.png");
+            Point[] pts = {
+                new Point(0, 100), new Point(200, 100), new Point(0, 0)
+            };
+            g.DrawImage(img, pts);
+        }
+    }
+}
