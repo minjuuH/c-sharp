2030316303e013fdab56d32008bff245b21a8709
minjuuH <hwangmj1124@gmail.com>
2022-11-29 23:18:13 +0900

    chapter 12 예제
    StringFormatSample
    DrawString 메소드 사용 예제

---
+++ chapter 12/StringFormatSample/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StringFormatSample {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            string s = "This is a long string that will wrap. ";
+            s += "It will be centered both vertically and horizontally.";
+            Font f = new Font("Tahoma", 15);
+            StringFormat sf = new StringFormat();
+
+            //가운데 정렬
+            sf.Alignment = StringAlignment.Center; // 수평 정렬
+            sf.LineAlignment = StringAlignment.Center; // 수직 정렬
+
+            g.DrawString(s, f, Brushes.Black, ClientRectangle, sf);
+            f.Dispose();
+
+        }
+    }
+}
