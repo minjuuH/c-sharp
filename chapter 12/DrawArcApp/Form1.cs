7f0c05689e8c4a2870c8ff1a94245e07f946fae3
minjuuH <hwangmj1124@gmail.com>
2022-11-29 23:02:52 +0900

    chapter 12 예제
    DrawArcApp
    부채꼴 그리기 예제

---
+++ chapter 12/DrawArcApp/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawArcApp {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Rectangle r = new Rectangle(50, 50, 150, 100);
+            g.FillPie(Brushes.LightGreen, r, 45, 270);
+            g.DrawPie(Pens.DarkGreen, r, 45, 270);
+        }
+    }
+}
