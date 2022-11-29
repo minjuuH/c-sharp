b759cb4d1aeea0d3d30c493c672913021b200dba
minjuuH <hwangmj1124@gmail.com>
2022-11-29 23:25:14 +0900

    chapter 12 예제
    DrawImageApp-1
    이미지 그리기 예제

---
+++ chapter 12/DrawImageApp-1/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawImageApp_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            //지정 좌표에 그림 - 원본의 크기로 출력
+            Graphics g = CreateGraphics();
+            Image img = new Bitmap("C:\\Users\\hwang\\source\\repos\\C#\\chapter 12\\vs.png");
+            //g.DrawImage(img, 0, 0); 
+            g.DrawImage(img, new Point(0, 0));
+        }
+    }
+}
