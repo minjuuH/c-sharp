6361877c42b0e9563291999ac2f71b9ab1fbaafe
minjuuH <hwangmj1124@gmail.com>
2022-11-29 23:15:25 +0900

    chapter 12 예제
    DrawStringSample
    DrawString 메소드 사용 예제

---
+++ chapter 12/DrawStringSample/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawStringSample {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            string s = "This string is long enough to wrap.";
+            s += " With a 250px-width rectangle, ";
+            s += "it requires six lines to display the string in its entirety.";
+            Font f = new Font("Tahoma", 15);
+            Rectangle r = new Rectangle(10, 10, 250, 150);
+            g.DrawRectangle(Pens.Black, r);
+            g.DrawString(s, f, Brushes.Black, r);
+            f.Dispose();
+        }
+    }
+}
