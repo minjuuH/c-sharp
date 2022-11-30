0cf57b1ad9ff9e45e41c80c28f7a4405038ebe53
minjuuH <hwangmj1124@gmail.com>
2022-11-30 12:13:03 +0900

    chapter 10 예제
    Mouse Paint
    마우스 이벤트(MouseMove) 사용 예제(그림판)

---
+++ chapter 10/Mouse Paint/frmMousePaint.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mouse_Paint
+{
+    public partial class frmMousePaint : Form
+    {
+        //Graphics 객체 참조 변수 선언
+        private Graphics m_objGraphics;
+
+        public frmMousePaint()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMousePaint_Load(object sender, EventArgs e)
+        {
+            m_objGraphics = this.CreateGraphics();
+        }
+
+        private void frmMousePaint_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            m_objGraphics.Dispose();    //그래픽 객체 자원 반납
+        }
+
+        private void frmMousePaint_MouseMove(object sender, MouseEventArgs e)
+        {
+            Rectangle rectEllipse = new Rectangle();    //원 외접 사각형
+
+            if (e.Button != MouseButtons.Left) 
+                return; // 왼쪽 마우스 버튼이 아닐 경우 바로 종료
+
+            //타원을 그리는 영역 설정
+            rectEllipse.X = e.X - 1;
+            rectEllipse.Y = e.Y - 1;
+            rectEllipse.Width = 2;
+            rectEllipse.Height = 2;
+
+            //원 그리기
+            m_objGraphics.DrawEllipse(System.Drawing.Pens.Blue, rectEllipse);
+        }
+    }
+}
