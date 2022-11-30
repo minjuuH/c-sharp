f819b96bb6d09c1271a4ff638f0aa33b9166814a
minjuuH <hwangmj1124@gmail.com>
2022-11-30 11:13:20 +0900

    chapter 10 예제
    ch10-04
    폼 안에 마우스가 들어가면 해당 마우스의 위치를 메시지박스로 출력시키는 예제

---
+++ chapter 10/ch10-04/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ch10_04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            Point mousePoint = PointToClient(MousePosition);    //마우스의 상대좌표 정보 저장
+            string msg = "Mouse Position : "+mousePoint.X+", "+mousePoint.Y;
+            MessageBox.Show(msg);   //마우스 위치를 메시지박스로 출력
+        }
+    }
+}
