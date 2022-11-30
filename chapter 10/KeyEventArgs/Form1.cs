f7b9ab05142792e2dab2d792b900eb8872c824e8
minjuuH <hwangmj1124@gmail.com>
2022-11-30 12:00:48 +0900

    chapter 10 예제
    KeyEventArgs
    키보드 이벤트(KeyDown) 사용 예제

---
+++ chapter 10/KeyEventArgs/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyEventArgs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+            messageBoxCS.AppendFormat("{0} = {1}", "Alt", e.Alt);
+            messageBoxCS.AppendLine();
+            messageBoxCS.AppendFormat("{0} = {1}", "Control", e.Control);
+            messageBoxCS.AppendLine();
+            messageBoxCS.AppendFormat("{0} = {1}", "Handled", e.Handled);
+            messageBoxCS.AppendLine();
+            messageBoxCS.AppendFormat("{0} = {1}", "KeyCode", e.KeyCode);
+            messageBoxCS.AppendLine();
+            messageBoxCS.AppendFormat("{0} = {1}", "KeyValue", e.KeyValue);
+            messageBoxCS.AppendLine();
+            messageBoxCS.AppendFormat("{0} = {1}", "KeyData", e.KeyData);
+            messageBoxCS.AppendLine();
+            messageBoxCS.AppendFormat("{0} = {1}", "Modifiers", e.Modifiers);
+            messageBoxCS.AppendLine();
+            messageBoxCS.AppendFormat("{0} = {1}", "Shift", e.Shift);
+            messageBoxCS.AppendLine();
+            MessageBox.Show(messageBoxCS.ToString(), "KeyDown Event");
+        }
+    }
+}
