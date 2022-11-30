0248f7e4f08db9fd0ab8a5f10f2a1edfedf49c97
minjuuH <hwangmj1124@gmail.com>
2022-11-30 11:42:28 +0900

    chapter 10 예제
    ch10-07
    입력 포커스를 이동시키기 위한 Focus() 메소드 사용 예제

---
+++ chapter 10/ch10-07/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ch10_07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //버튼1 클릭 시 버튼2가 입력 포커스를 가짐
+            this.button1.BackColor = SystemColors.Control;
+            this.button2.Focus();
+            if (this.button2.Focused)
+                this.button2.BackColor = SystemColors.ControlDark;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //버튼2 클릭 시 버튼1이 입력 포커스를 가짐
+            this.button2.BackColor = SystemColors.Control;
+            this.button1.Focus();
+            if (this.button1.Focused)
+                this.button1.BackColor = SystemColors.ControlDark;
+        }
+    }
+}
