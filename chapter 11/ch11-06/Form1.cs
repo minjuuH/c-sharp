3dc05695e5a2338884ac339a975e4a4e080c4fc0
minjuuH <hwangmj1124@gmail.com>
2022-11-30 12:31:03 +0900

    chapter 11 예제
    ch11-06
    트랙바 사용 예제

---
+++ chapter 11/ch11-06/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch11_06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = trackBar1.Value;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            trackBar1.Value = (int)numericUpDown1.Value;
+            // 수치적 업다운 컨트롤의 Value프로퍼티는 Decimal 형
+            // trackBar의 Value 프로퍼티는 정수형
+        }
+
+    }
+}
