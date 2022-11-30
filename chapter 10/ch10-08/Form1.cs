05afc2d3b399d1914cf3b4b4247463347713b9b3
minjuuH <hwangmj1124@gmail.com>
2022-11-30 11:48:16 +0900

    chapter 10 예제
    ch10-08
    컨트롤이 입력 포커스를 받았을 때 가질 Enter 이벤트 사용 예제

---
+++ chapter 10/ch10-08/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ch10_08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Enter: 자신 또는 자식 컨트롤이 입력포커스를 가질 때 발생
+        //Leave: 자신 또는 자식 컨트롤이 입력포커스를 잃을 때 발생
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("비밀번호를 입력하세요");
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("두번째 TextBox 입력");
+        }
+    }
+}
