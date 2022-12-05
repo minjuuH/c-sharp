da7889d3d5a3cf225a8ecaabb0484107a2a78cdb
minjuuH <hwangmj1124@gmail.com>
2022-12-06 02:08:29 +0900

    ch11-02
    - 트리뷰 사용 예제

---
+++ chapter 11/ch11-02/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch11_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            treeView1.ExpandAll();      //트리 뷰의 모든 노드를 펼침
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && treeView1.SelectedNode != null) {
+                // 선택된 노드가 있으면, 그 노드의 자식 노드로 추가한다.
+                treeView1.SelectedNode.Nodes.Add(new TreeNode(textBox1.Text, 1, 1));
+                textBox1.Text = "";
+                textBox1.Focus(); // 키 입력 가능
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Remove(treeView1.SelectedNode);
+        }
+    }
+}
